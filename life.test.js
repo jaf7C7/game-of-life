8b6b523acff2579f450593d48fcb4a72d59a8fcf
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-11-20 07:52:15 +0000

    JavaScript does not have tuples!

--- life.test.js
+++ life.test.js
@@ -1,6 +1,6 @@
 import { next } from './life.js'
 
 test('a live cell with no neighbours dies', () => {
-    cells = [(0, 0)];
+    cells = [[0, 0]];
     expect(next(cells)).toStrictEqual([]);
 });
