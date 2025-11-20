edc1288d7b4aa4e6e72cf3cef20429de1e64a0ac
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-11-20 07:31:47 +0000

    Fix error in test assertion

--- life.test.js
+++ life.test.js
@@ -2,5 +2,5 @@ import { next } from './life.js'
 
 test('a live cell with no neighbours dies', () => {
     cells = [(0, 0)];
-    expect(next(cells)).toBe([]);
+    expect(next(cells)).toStrictEqual([]);
 });
