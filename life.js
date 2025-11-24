dd31ba97dc622d69a94bc5529edbfafb1390e74e
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-11-24 09:51:20 +0000

    Refactoring: Extract function
    Neighbours are now calculated dynamically instead of being hard-coded. We still
    only check neighbours of [0, 0] so this implementation must be made more
    general.

--- life.js
+++ life.js
@@ -6,17 +6,24 @@ function cellIsAlive(targetCell, cells) {
     return cells.find((cell) => cellsAreEqual(targetCell, cell)) !== undefined;
 }
 
-export function next(cells) {
-    const potentialNeighbours = [
-        [1, 0],
-        [1, 1],
-        [0, 1],
-        [1, -1],
-        [-1, 0],
-        [-1, -1],
-        [0, -1],
-        [1, -1],
+function getNeighbours(targetCell) {
+    const [x, y] = targetCell;
+    const neighbours = [
+        [x + 1, y],
+        [x + 1, y + 1],
+        [x, y + 1],
+        [x - 1, y + 1],
+        [x - 1, y],
+        [x - 1, y - 1],
+        [x, y - 1],
+        [x + 1, y - 1],
     ];
+
+    return neighbours;
+}
+
+export function next(cells) {
+    const potentialNeighbours = getNeighbours([0, 0]);
     const liveNeighbours = potentialNeighbours.filter((potentialNeighbour) =>
         cellIsAlive(potentialNeighbour, cells),
     );
