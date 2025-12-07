1790003cd6aecea55c9e0b71a6d22c11f368d355
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-12-07 16:07:08 +0000

    Refactoring: Remove `createGrid` function

--- ui.js
+++ ui.js
@@ -1,23 +1,5 @@
 import { equals, next } from './life.js';
 
-export function createGrid() {
-    return {
-        cells: [],
-        toggleCell(x, y) {
-            const targetCell = [x, y];
-            const cellIndex = this.cells.findIndex((cell) =>
-                equals(cell, targetCell),
-            );
-
-            if (cellIndex === -1) {
-                this.cells.push([x, y]);
-            } else {
-                this.cells.splice(cellIndex, 1);
-            }
-        },
-    };
-}
-
 export function createGame() {
     let cells = [];
     let playing = false;
