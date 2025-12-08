0f0e68d33eddcd79a448b0f37e1df66cb81951ca
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-12-08 15:13:42 +0000

    Refactoring: Slide statement

--- tests/life.test.js
+++ tests/life.test.js
@@ -30,6 +30,16 @@ test('the game is initially in stopped state', () => {
     expect(life.isPlaying()).toBe(false);
 });
 
+test('the game calculates new state of the grid with each tick', () => {
+    // A lone cell without neighbours will die after 1 generation.
+    const life = new Life();
+    life.toggleCell(5, 5);
+
+    life.tick();
+
+    expect(life.cells).toEqual([]);
+});
+
 test('the game loop can be started', () => {
     const life = new Life();
     let tickCallback = null;
@@ -76,14 +86,4 @@ test('the game loop can be stopped', () => {
 
     expect(life.isPlaying()).toBe(false);
     expect(loopCancelled).toBe(true);
-});
-
-test('the game calculates new state of the grid with each tick', () => {
-    // A lone cell without neighbours will die after 1 generation.
-    const life = new Life();
-    life.toggleCell(5, 5);
-
-    life.tick();
-
-    expect(life.cells).toEqual([]);
 });
