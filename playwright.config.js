8532f036db04b7718264761ad7b3a00bbbbfb029
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-12-09 17:25:15 +0000

    Configure playwright

--- playwright.config.js
+++ playwright.config.js
@@ -1,22 +1,18 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
-/** Read environment variables from file. https://github.com/motdotla/dotenv */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
 /** @see https://playwright.dev/docs/test-configuration */
 export default defineConfig({
     testDir: './tests',
+    testMatch: 'ui.test.js',
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    reporter: 'list',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('')`. */
-        // baseURL: 'http://localhost:3000',
+        baseURL: 'http://localhost:3000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
@@ -61,8 +57,8 @@ export default defineConfig({
     ],
 
     /* Run your local dev server before starting the tests */
-    // webServer: {
-    //   command: 'npm run start',
-    //   url: 'http://localhost:3000',
-    // },
+    webServer: {
+        command: 'npm run start',
+        url: 'http://localhost:3000',
+    },
 });
