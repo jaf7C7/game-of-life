5cbaa2538609b8c277b00f6e34694a93f5266074
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-12-11 08:17:09 +0000

    Update UI: Refactor grid rendering logic

--- life/ui.js
+++ life/ui.js
@@ -2,24 +2,21 @@ export default function render() {
     const grid = document.querySelector('#grid');
     const ctx = grid.getContext('2d');
 
-    const gridHeight = 1000;
-    const gridWidth = 500;
+    const gridSize = 10;
     const cellSize = 25;
 
     ctx.strokeStyle = 'black';
     ctx.lineWidth = 0.5;
 
-    for (let y = 0; y <= gridHeight; y += cellSize) {
+    for (let i = 0; i <= gridSize; i++) {
         ctx.beginPath();
-        ctx.moveTo(0, y);
-        ctx.lineTo(gridWidth, y);
-        ctx.stroke();
-    }
 
-    for (let x = 0; x <= gridWidth; x += cellSize) {
-        ctx.beginPath();
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, gridHeight);
+        ctx.moveTo(i * cellSize, 0);
+        ctx.lineTo(i * cellSize, gridSize * cellSize);
+
+        ctx.moveTo(0, i * cellSize);
+        ctx.lineTo(gridSize * cellSize, i * cellSize);
+
         ctx.stroke();
     }
 }
